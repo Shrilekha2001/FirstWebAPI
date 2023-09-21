d69b2ee381cc1b51e5fb2b0aa4112ea993cf3e9d
shrilekha2001 <shrilekha.j@gavstech.com>
2023-09-21 14:09:36 +0530

    included book and book controller

---
+++ FirstWebAPI/Controllers/BookController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using FirstWebAPI.NewFolder;
+
+namespace FirstWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+         static List<Book> bookList = new List<Book>();
+        [HttpGet]
+        public List<Book> GetBooks()
+        {
+            List<Book> bookList = new List<Book>();
+            for(int i = 0; i < 5; i++)
+            {
+                Book book = new Book();
+                book.BookID = i;
+                book.Title = "Atomic Habits" + i;
+                book.Cost = i * 100;
+                bookList.Add(book);
+            }
+            return bookList;
+        }
+        [HttpPost]
+        public int AddBook(Book book)
+        {
+            bookList.Add(book);
+            return 1;
+
+        }
+    }
+}
